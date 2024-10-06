4e9912eac8cb06a5f8bcc7b3f00112dc32c23246
erichester76 <eric.hester@gmail.com>
2024-10-05 21:57:14 -0400

    Update __init__.py

--- netbox_loadbalancer/__init__.py
+++ netbox_loadbalancer/__init__.py
@@ -6,6 +6,7 @@ class NetboxLoadBalancerConfig(PluginConfig):
     description = 'Manager LoadBalancer with Netbox'
     version = '1.0.0'
     author = 'HuyTM'
+    min_version = '4.0.0'
     author_email = 'huytm@hocchudong.com'
     base_url = 'netbox_loadbalancer'
 
